4195bfbc7df2a27540e21068168fa2f49715f25d
Burak Kısacık <burakkisacik@yahoo.com>
2025-01-13 20:52:25 +0300

    feat: add tmux-sessionizer.sh script

---
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# you need to export the path in your .rc file in order to run this script
+# globally from everywhere
+# example --> export PATH="$HOME/.config/bin/.local/scripts:$PATH"
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(find ~/projects -mindepth 1 -maxdepth 1 -type d | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux attach-session -t $selected_name
+else
+    tmux swith-client -t $selected_name
+fi
